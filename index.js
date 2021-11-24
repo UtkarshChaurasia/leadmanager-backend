760dc8ac6fd75096f437783afa065ef7f77714eb
UtkarshChaurasia <msdianuc07@gmail.com>
2021-11-24 14:54:42 +0530

    auth api refactor

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ connectToMongo();
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(express.json())
 
--- routes/auth.js
+++ routes/auth.js
@@ -4,28 +4,40 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 
-// Create a User using: POST "/api/auth/", Doesn't require Auth
-router.post('/', [
+// Create a User using: POST "/api/auth/createuser". No login required
+router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a email name').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-], (req, res) => {
+], async (req, res) => {
+
+    // If there are errors, return Bad Request and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user))
-        .catch(err => {
-            console.error(err.message);
-            res.json({ error: 'Please enter a unique value for email', message: err.message })
+
+    
+    try {
+        // Check whether the user with this email exist already
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "sorry a user with this email already exists" })
+        }
+
+        // Create a new User
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
         })
+
+        res.json(user);
+    }
+    catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some error occurred");
+    }
 });
 
 module.exports = router;
-
-
-//7860830163
